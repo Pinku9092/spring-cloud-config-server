5d554a7169d1e2ef4b12415cc5d329093765241f
Pinku <kpinku32@gmail.com>
2020-09-06 23:51:44 +0530

    spring-cloud-config-server repo

---
+++ src/test/java/com/pinku/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinku.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
